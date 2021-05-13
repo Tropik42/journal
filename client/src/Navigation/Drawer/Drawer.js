fc3e02d8b30904b6174114eb2fa62f17e0e09839
Tropik42 <aviavova94@gmail.com>
2021-05-13 16:53:15 +0300

    calendar commented

--- client/src/Navigation/Drawer/Drawer.js
+++ client/src/Navigation/Drawer/Drawer.js
@@ -7,7 +7,7 @@ const links = [
         {to: '/Todos', label: 'Зарубки', exact: true}, 
         {to: '/Links', label: 'Закладки', exact: false}, 
         {to: '/Lists', label: 'Списки', exact: false}, 
-        {to: '/Calendar', label: 'Календарь', exact: false}, 
+        // {to: '/Calendar', label: 'Календарь', exact: false}, 
         {to: '/Persons', label: 'Люди', exact: false}, 
     ]
 
